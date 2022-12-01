a88f7b65249d31edd2fd037a522f8298b6e91c00
davidgregs87 <udohdavid27@gmail.com>
2022-12-01 12:36:37 -0800

    updated

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+from sys import argv
+
 if __name__ == "__main__":
-    import sys
-    res = 0
-    for str in range(len(sys.argv)):
-        if (str == 0):
-            continue
-        else:
-            res += int(sys.argv[str])
-    print("{}".format(res))
+        sum = 0
+
+        for number in argv[1:]:
+            sum = sum + int(number)
+        print("{:d}".format(sum))
