8aa831a0938186c7da35ba717f5a2a4f26d37920
davidgregs87 <udohdavid27@gmail.com>
2023-05-16 15:27:24 -0700

    Don’t list rows without a name value

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Write a script that lists all records of the table second_table of the database
+-- Don’t list rows without a name value
+SELECT score, name  FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
