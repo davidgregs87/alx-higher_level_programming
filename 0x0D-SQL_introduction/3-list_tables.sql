6906433bffd05f3ce7efb62303dcb30b86c6336a
davidgregs87 <udohdavid27@gmail.com>
2023-05-16 08:22:27 -0700

    list mysql tables

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,4 @@
 -- mysql script to list tables
 -- first you have to show databases and select the database we which to use
--- then we list the table of that database
-USE mysql;
+-- then we list the table of that databas
 SHOW TABLES;
