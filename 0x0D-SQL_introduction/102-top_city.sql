0b50bf8754fce075f84fd44291bcf10fbce19709
davidgregs87 <udohdavid27@gmail.com>
2023-05-16 16:21:45 -0700

    Retrieved average temperatures for cities between month 7 or month 8

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- Retrieves the top 3 of cities temperature during July and August ordered by temperature
+SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month=7 or  month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
