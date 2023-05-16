2fe637e1fbfbc5ce0429d172ea05ddfb7bf40364
davidgregs87 <udohdavid27@gmail.com>
2023-05-16 08:46:47 -0700

    created my first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,2 @@
 -- ceating our first table call first_table
-CREATE TABLE IF NOT EXISTS first_table(
-	id INT,
-	name VARCHAR(256),
-);
-
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
