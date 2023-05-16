45611684794fc0ffca57089aa99a93ea84cdfaf3
davidgregs87 <udohdavid27@gmail.com>
2023-05-16 13:55:20 -0700

    Calculates the average score from the database

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- calculate the average of the score column of the database
+-- AVG is a built for calculating average, and save's it as a variable  "average"
+SELECT AVG(score) as average FROM second_table;
