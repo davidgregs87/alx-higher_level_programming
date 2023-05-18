668a1b54334707db74962460232d0b4aad0c8c61
davidgregs87 <udohdavid27@gmail.com>
2023-05-18 08:54:53 -0700

    added FK contraint to reference parent table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- Add contraints to the table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL, FOREIGN KEY(id) REFERENCES states(id),name VARCHAR(256) NOT NULL);
