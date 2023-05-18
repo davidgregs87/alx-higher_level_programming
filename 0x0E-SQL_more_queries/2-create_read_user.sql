f136e1c4cb629c96a9576f03c80651d7bd67cd54
davidgregs87 <udohdavid27@gmail.com>
2023-05-18 06:12:53 -0700

    create database, create user and grant select permission

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
--- create database, create user and grant only select privilege to user
+-- create database, create user and grant the user select privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 USE hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2;
-SET PASSWORD TO 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
-GRANT SELECT PERMISSION ON hbtn_0d_0 TO 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
