e592246b0be821fc034db8ef1fe93844ea22c9e8
davidgregs87 <udohdavid27@gmail.com>
2023-05-18 01:41:49 -0700

    create user,set password and grant user all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- Create a new user, set their password, grant this user all permission
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON '*' TO  'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO  'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
 
