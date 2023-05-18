4b2eab3725f5ea8dc2eed21f6484317ea66613b4
davidgregs87 <udohdavid27@gmail.com>
2023-05-18 07:37:08 -0700

    if not null

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- Enter varchar if not null
-CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) IF NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
 
