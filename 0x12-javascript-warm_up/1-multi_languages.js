ea9287193b466765b2227c6194e91053ca02d74f
davidgregs87 <udohdavid27@gmail.com>
2023-06-13 21:18:06 +0000

    prints out multiple languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+console.log(myVar1);
+console.log(myVar2);
+console.log(myVar3);
