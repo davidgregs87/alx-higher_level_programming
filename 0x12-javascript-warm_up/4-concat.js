9606d19a4da10a183e7f3448b5df44bb4cd12655
davidgregs87 <udohdavid27@gmail.com>
2023-06-14 02:10:04 +0000

    concatenate command line strings

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myArg = process.argv;
+myArg[0] = 'undefined';
+myArg[1] = 'undefined';
+if (!myArg[2]) {
+  console.log(myArg[0]  + ' is ' +  myArg[1]);
+} else if (myArg === 2) {
+  console.log(myArg[2] + ' is ' + myArg[1]);
+} else {
+  console.log(myArg[2] + ' is ' + myArg[3]);
+}
