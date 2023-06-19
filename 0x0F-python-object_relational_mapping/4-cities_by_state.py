071af3af7b5bc28dbacd2ee1d2a2a6d8a2bd75b5
davidgregs87 <udohdavid27@gmail.com>
2023-06-19 13:45:59 -0700

    join to tables and query them

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,9 @@ if __name__ == '__main__':
     cur = conn.cursor()
 
     # Execute query
-    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                ORDER BY cities.id ASC""")
     query = cur.fetchall()
 
     # Iterate through "query"
