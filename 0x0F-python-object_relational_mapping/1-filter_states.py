9f4747cc04f7eb6a07b78364b5fde95109188915
davidgregs87 <udohdavid27@gmail.com>
2023-06-19 10:54:39 -0700

    Filter search query

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Filter search query
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    uname = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    conn = MySQLdb.connect(host='localhost', user=uname, passwd=password,
+                           db=database, port=3306, charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE
+                'N%' ORDER BY id ASC")
+    query = cur.fetchall()
+    for rows in query:
+        print(rows)
+    cur.close()
+    conn.close()
