9595b60e8e32cb42657f5a7dd6089d1f8ec76e1d
davidgregs87 <udohdavid27@gmail.com>
2023-06-19 08:14:14 -0700

    Open a connection to a db and make a Query

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,14 +6,16 @@ Accessing a database(connection) and make a query
 import MySQLdb
 import sys
 
-# Assign the first, second and third argument to a variable which is to be passed to the connection below
+# Assign the first, second and third argument
+# to a variable which is to be passed to the connection below
 if __name__ == '__main__':
     uname = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
 # Replaced connection parameters with command line arguments variables
-    conn = MySQLdb.connect(host='localhost', port = 3306 , user = uname , passwd = password , db = database , charset = 'utf8')
+    conn = MySQLdb.connect(host='localhost', port=3306, user=uname,
+                           passwd=password, db=database, charset='utf8')
     cur = conn.cursor()
     cur.execute('SELECT * FROM states ORDER BY id ASC')
     query = cur.fetchall()
