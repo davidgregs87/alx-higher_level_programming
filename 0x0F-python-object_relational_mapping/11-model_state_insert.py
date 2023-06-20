6710c7f9e5d9ac6d017fc7b89cbc2feb5aa2a33e
davidgregs87 <udohdavid27@gmail.com>
2023-06-20 08:10:29 -0700

    Insert into state table and query its ID

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module is only meant to print the first line of a query
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+
+# Now let's set-up connection to a database
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    # Let's begin session
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    new_state = State()
+    new_state.name = "Louisiana"
+
+    session.add(new_state)
+
+    # Let's now begin query
+
+    info = session.query(State).filter(State.name == "Louisiana")
+    if not (info.first()):
+        print('Not found')
+    else:
+        for x in info:
+            print('{}'.format(x.id))
+    # Commit session
+    session.commit()
+    # Session closed
+    session.close()
