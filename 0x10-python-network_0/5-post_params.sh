3976ea5accf89467655097f9cb8ea95dbbffbabe
davidgregs87 <udohdavid27@gmail.com>
2023-06-30 19:25:58 -0700

    Sent a POST to the server

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sX POST -d "email%3A%20test@gmail.com&subject%3A%20I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
