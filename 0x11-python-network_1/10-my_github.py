b71c40eacae7b169d8511dcf5ccfa5f3fbdbfabb
davidgregs87 <udohdavid27@gmail.com>
2023-07-02 15:26:58 -0700

    Used my username and password as command line arguments to authenticate to github api using the requests package and it related attributes

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Using github api to authenticate with my username
+and password to retrieve my github id"""
+
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+if __name__ == '__main__':
+    uname = sys.argv[1]
+    passwd = sys.argv[2]
+    url = 'https://api.github.com/user'
+
+    req = requests.get(url, auth=HTTPBasicAuth(uname, passwd))
+    if req.status_code == 200:
+        r = req.json()
+        print("{}".format(r.get('id')))
+    else:
+        print("Failed to retrieve your information!")
