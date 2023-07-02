8df104a762c893a29190374ad6e24a96f0edf6e9
davidgregs87 <udohdavid27@gmail.com>
2023-07-02 05:17:08 -0700

    fetched the url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('UTF-8')))
