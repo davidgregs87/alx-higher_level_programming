27e1df862398019e3ba0f39b8eaadf4bb68db7c4
davidgregs87 <udohdavid27@gmail.com>
2023-07-02 13:11:40 -0700

    Retrieved the X-Request-Id of the URL

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,5 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     body = requests.get(url)
-    body_res = body.headers['X-Request-Id']
+    body_res = body.headers.get('X-Request-Id')
     print(body_res)
