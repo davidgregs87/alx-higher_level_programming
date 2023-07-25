2bdb33e5f0f0b8d7d973da953b19524b60e2ae0b
davidgregs87 <udohdavid27@gmail.com>
2023-07-25 22:33:28 +0000

    task4 [solved]

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/`;
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error.message);
+  } else {
+    const film = JSON.parse(body).results;
+    let count = 0;
+    for (const x in film) {
+      const character = film[x].characters;
+      for (const item in character) {
+        if (character[item].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
