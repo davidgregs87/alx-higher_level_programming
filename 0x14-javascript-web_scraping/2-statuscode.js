c8db2acac2da599794349926fc1c3c8909e55cdd
davidgregs87 <udohdavid27@gmail.com>
2023-07-25 21:32:31 +0000

    task2 [solved]

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error.message);
+  } else {
+    console.log(response.statusCode);
+  }
+});
