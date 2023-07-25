8cef822e1effa2f2f8b8e7134d544fcb08cacd4b
davidgregs87 <udohdavid27@gmail.com>
2023-07-25 21:56:50 +0000

    task3 [solved]

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error.message);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+});
