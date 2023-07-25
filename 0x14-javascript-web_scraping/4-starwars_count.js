c60a6bb54aebbef95667c0dfbbddf1d29f88f498
davidgregs87 <udohdavid27@gmail.com>
2023-07-25 22:40:39 +0000

    task4 solved

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/`;
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error.message);
