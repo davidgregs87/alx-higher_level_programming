3ccb46ece8b4bd2b0dfb1770b8e292e100619dcf
davidgregs87 <udohdavid27@gmail.com>
2023-07-25 18:07:32 +0000

    task0 [completed]

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filepath = process.argv[2];
+fs.readFile(filepath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log(data);
+  }
+});
