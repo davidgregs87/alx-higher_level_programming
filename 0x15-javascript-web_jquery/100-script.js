fe6430796faa0a1dc42900b5715a4d1a853590d9
davidgregs87 <udohdavid27@gmail.com>
2023-08-05 01:29:28 +0100

    done

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+(() => {
+    document.addEventListener('DOMContentLoaded', () => {
+        const elem = document.querySelector('header');
+        elem.style.color = '#FF0000';
+    }, false);
+})();
